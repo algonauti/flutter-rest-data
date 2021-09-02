4ac663ef99344a80711b5e1cc708ab042daf394b
Artem Kalachyan <artem.kalachyan@hotmail.com>
2021-09-02 15:18:39 +0300

    Update to the latest dart-rest-data version

--- example/lib/data/beer_model.dart
+++ example/lib/data/beer_model.dart
@@ -4,17 +4,17 @@ import 'package:flutter_rest_data_example/data/ingredient_model.dart';
 class BeerModel extends JsonApiModel {
   BeerModel(JsonApiDocument jsonApiDoc) : super(jsonApiDoc);
 
-  String get name => attributes!['name'];
+  String get name => attributes['name'];
 
-  String get tagline => attributes!['tagline'];
+  String get tagline => attributes['tagline'];
 
-  String get description => attributes!['description'];
+  String get description => attributes['description'];
 
-  String get imageUrl => attributes!['image_url'];
+  String get imageUrl => attributes['image_url'];
 
-  String get alcoholByVolume => attributes!['alcohol_by_volume'];
+  String get alcoholByVolume => attributes['alcohol_by_volume'];
 
-  List<String> get foodPairing => attributes!['food_pairing'].cast<String>();
+  List<String> get foodPairing => attributes['food_pairing'].cast<String>();
 
   Iterable<IngredientModel> get ingredients =>
       includedDocs("ingredients").map((jsonDoc) => IngredientModel(jsonDoc));
--- example/lib/data/ingredient_model.dart
+++ example/lib/data/ingredient_model.dart
@@ -3,11 +3,11 @@ import 'package:flutter_rest_data/flutter_rest_data.dart';
 class IngredientModel extends JsonApiModel {
   IngredientModel(JsonApiDocument jsonApiDoc) : super(jsonApiDoc);
 
-  String get kind => attributes!['kind'];
+  String get kind => attributes['kind'];
 
-  String get name => attributes!['name'];
+  String get name => attributes['name'];
 
-  String get quantity => attributes!['qty'];
+  String get quantity => attributes['qty'];
 
-  String get unit => attributes!['unit'];
+  String get unit => attributes['unit'];
 }
